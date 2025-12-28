770f4694be458d90df156fbbce5181853066b9de
Bangdams <sadamjr67@gmail.com>
2025-12-28 19:59:07 +0700

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
+import { useEffect } from "react";
 
 export function HomePage() {
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      console.log(response.data);
+    });
+  });
+
   return (
     <>
       <title>Ecommerce Project</title>
